b856c1a15b3cbf557cba4872394d582753b733c7
Pattanan Aphiwetsa <monz@Pattanans-MacBook-Pro.local>
2024-05-03 18:40:24 -0700

    xcode init

---
+++ mac-calendar-manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mac-calendar-manager
+//
+//  Created by Pattanan Aphiwetsa on 3/5/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Welcome to Calendar Management Powered by LLM")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mac-calendar-manager/mac_calendar_managerApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_calendar_managerApp.swift
+//  mac-calendar-manager
+//
+//  Created by Pattanan Aphiwetsa on 3/5/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct mac_calendar_managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
